fef0955e9a3fe9a1353a793ac0d06c90a9abe1fc
QW3 <contato@qw3.com.br>
2011-09-02 11:37:49 -0300

    Pagination generator

--- lib/generators/qw3common/pagination/pagination_generator.rb
+++ lib/generators/qw3common/pagination/pagination_generator.rb
@@ -4,8 +4,23 @@ module Qw3common
       
       source_root File.expand_path("../templates", __FILE__)
       
-      def create_javascript_files
-        copy_file "public/javascript/pagination.js", "public/javascript/qw3common/pagination.js"
+      def create_config_files
+        #initializers
+        copy_file "config/initializers/pagination.rb", "config/initializers/qw3common/pagination.rb"
+        #locales
+        copy_file "config/locales/pagination.en.yml", "config/initializers/qw3common.pagination.en.yml"
+        copy_file "config/locales/pagination.pt-BR.yml", "config/initializers/qw3common.pagination.pt-BR.yml"
+      end
+      
+      def create_public_files
+        #javascripts
+        copy_file "public/javascripts/pagination.js", "public/javascript/qw3common/pagination.js"
+        #stylesheets
+        copy_file "public/stylesheets/pagination.css", "public/javascript/qw3common/pagination.css"
+      end
+      
+      def create_helpers_files
+        copy_file "helpers/pagination_helper.rb", "app/helpers/pagination_helper.rb"
       end
       
     end
