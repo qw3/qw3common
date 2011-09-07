00da0d34548b4e30899cb88fb1cca369c642c188
QW3 <contato@qw3.com.br>
2011-09-07 10:59:56 -0700

    Tradução para o Devise

--- lib/generators/qw3common/template/template_generator.rb
+++ lib/generators/qw3common/template/template_generator.rb
@@ -10,6 +10,7 @@ module Qw3common
         if yes?('Gostaria de criar o usuário do Devise?')
           gem 'devise'
           generate 'devise:install'
+          copy_file 'config/locales/devise.pt-BR.yml', 'config/locales/devise.pt-BR.yml'
           model_name = ask('Qual o nome do usuário do Devise?')
           model_name = 'administrador' if model_name.blank?
           generate 'devise', model_name
