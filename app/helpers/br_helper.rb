fbc308020652d47f8287a2d87ec14ce2d1c4aa6b
QW3 <contato@qw3.com.br>
2011-09-12 15:53:54 -0300

    Arrumado BrHelper

--- app/helpers/br_helper.rb
+++ app/helpers/br_helper.rb
@@ -3,6 +3,8 @@ module BrHelper
   def create_alias apelido, nome
     if apelido.blank?
       return nome.remover_acentos.downcase.gsub(/ /, "_")
+    else
+      return apelido
     end
   end
   
