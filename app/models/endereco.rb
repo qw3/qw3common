a0755da8a13c6c5a0866c20f244d63fb4b9cba54
Marcelo Theodoro <marcelo@qw3.com.br>
2011-10-06 10:38:00 -0300

    bairro tem filtro de cidade

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -4,8 +4,8 @@ class Endereco < ActiveRecord::Base
   
   validates :cidade_id, :rua, :cep, :presence => true
   
-  def Endereco.bairros(cidade_id = nil)
-    conditions = " AND cidade_id = #{cidade_id}" unless cidade_id.nil?
+  def Endereco.bairros(cidade = nil)
+    conditions = " AND cidade_id = #{cidade.id}" unless cidade_id.nil?
     bairros = Endereco.select( 'distinct(bairro)' ).where( "bairro IS NOT NULL #{conditions}" ).order('bairro ASC')
     bairros_array = []
     bairros.each do |b|
