100f77c6924b1f0914969d02ed14bf82b385fa45
Marcelo Theodoro <marcelo@qw3.com.br>
2011-10-06 10:29:16 -0300

    Endereço tem listagem de bairros

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -4,4 +4,14 @@ class Endereco < ActiveRecord::Base
   
   validates :cidade_id, :rua, :cep, :presence => true
   
+  def Endereco.bairros(cidade_id = nil)
+    conditions = " AND cidade_id = #{cidade_id}" unless cidade_id.nil?
+    bairros = Endereco.select( 'distinct(bairro)' ).where( "bairro IS NOT NULL #{conditions}" ).order('bairro ASC')
+    bairros_array = []
+    bairros.each do |b|
+      bairros_array.push b.bairro
+    end
+    return bairros_array
+  end
+   
 end
