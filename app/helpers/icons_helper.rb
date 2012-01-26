0b31012652dd95e58ae97920316c852f32ff8434
QW3 <contato@qw3.com.br>
2012-01-26 17:15:37 -0200

    Icons

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -3,13 +3,14 @@ module BrHelper
   
   def icon_path( file_name, size )
   
-    exts = { "bmp", "css", "csv", "doc", "docx", "eps", "gif", "html", "ico", "jpg", "js", "json", "mp3", "pdf", "php", "png", "ppt", "pptx", "psd", "svg", "swf", "tiff", "txt", "wav", "xls", "xlsx", "xml" }
+    exts = [ "bmp", "css", "csv", "doc", "docx", "eps", "gif", "html", "ico", "jpg", "js", "json", "mp3", "pdf", "php", "png", "ppt", "pptx", "psd", "svg", "swf", "tiff", "txt", "wav", "xls", "xlsx", "xml" ]
     path = "images/icons/documents"
   
     # recuperando a extensão do arquivo
     file_name = file_name.split( "." )
     # Caso não tenha extensão, ícone padrão é o de txt
-    if( file_name.length.eql? 1 )
+    # Caso não exista o ícone da extensão, icone padrão é o do txt
+    if file_name.length.eql?(1) or !exts.include?(file_name[ file_name.length - 1])
       ext = "txt"
     else
       ext = file_name[ file_name.length - 1]
