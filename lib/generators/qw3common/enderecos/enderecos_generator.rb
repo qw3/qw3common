16af366dd205b6dccb80242d254cc1a56add11c1
QW3 <contato@qw3.com.br>
2012-04-13 14:52:32 -0300

    .

--- lib/generators/qw3common/enderecos/enderecos_generator.rb
+++ lib/generators/qw3common/enderecos/enderecos_generator.rb
@@ -15,7 +15,7 @@ module Qw3common
       # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
       def self.next_migration_number(dirname)
         if ActiveRecord::Base.timestamped_migrations
-          Time.now.utc.strftime("%Y%m%d%H%M%S").to_s + rand(1230).to_s
+          Time.now.utc.strftime("%Y%m%d%H%M%S").to_s
         else
           "%.3d" % (current_migration_number(dirname) + 1)
         end
